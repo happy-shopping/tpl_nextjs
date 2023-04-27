089e647643457907d9724f7efb81dd882c8ca7fd
우진택 <jt.woo@pping.kr>
2023-04-27 17:20:13 +0900

    eslint 설치 및 세팅, prettier 설치 및 세팅

--- next.config.js
+++ next.config.js
@@ -1,25 +1,24 @@
 /** @type {import('next').NextConfig} */
-let BUILD_DIR = 'loc';
+let BUILD_DIR = "loc";
 
 switch (process.env.NEXT_PUBLIC_TLD) {
-  case 'loc':
-    BUILD_DIR = 'build/loc';
+  case "loc":
+    BUILD_DIR = "build/loc";
     break;
-  case 'dev':
-    BUILD_DIR = 'build/dev';
+  case "dev":
+    BUILD_DIR = "build/dev";
     break;
-  case 'kr':
-    BUILD_DIR = 'build/main';
+  case "kr":
+    BUILD_DIR = "build/main";
     break;
   default:
-    BUILD_DIR = 'build/loc';
+    BUILD_DIR = "build/loc";
 }
-
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: BUILD_DIR,
   trailingSlash: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
