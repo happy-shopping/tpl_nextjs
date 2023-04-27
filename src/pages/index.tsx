f0ffad9472ea66006768804a58f8711e0a424845
우진택 <jt.woo@pping.kr>
2023-04-27 18:52:17 +0900

    dayjs 설치 및 사용 예제 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>행복쇼핑 Next.js Pages 폴더 구조 템플릿</h1>
+      <li>
+        <Link href="/dayjs">dayjs 페이지</Link>
+      </li>
     </main>
   );
 }
