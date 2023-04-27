71c2eae7599ae44ff8e5205682d6bc429571e9f6
우진택 <jt.woo@pping.kr>
2023-04-27 16:52:10 +0900

    env 파일 생성, build 파일 생성

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+let BUILD_DIR = 'loc';
+
+switch (process.env.NEXT_PUBLIC_TLD) {
+  case 'loc':
+    BUILD_DIR = 'build/loc';
+    break;
+  case 'dev':
+    BUILD_DIR = 'build/dev';
+    break;
+  case 'kr':
+    BUILD_DIR = 'build/main';
+    break;
+  default:
+    BUILD_DIR = 'build/loc';
+}
+
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  distDir: BUILD_DIR,
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
