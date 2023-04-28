ff67440b39ecc297c9238c51366e8751200f4c13
우진택 <jt.woo@pping.kr>
2023-04-28 13:43:44 +0900

    redux redux-toolkit 설치 후 빌드 파일 생성

--- build/dev/server/pages/_app.js
+++ build/dev/server/pages/_app.js
@@ -21,8 +21,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _styles_theme__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(1278);
 /* harmony import */ var _tanstack_query_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(6929);
 /* harmony import */ var _tanstack_react_query__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(9752);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(6022);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(react_redux__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var _store__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(9224);
 var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_tanstack_query_core__WEBPACK_IMPORTED_MODULE_4__, _tanstack_react_query__WEBPACK_IMPORTED_MODULE_5__]);
 ([_tanstack_query_core__WEBPACK_IMPORTED_MODULE_4__, _tanstack_react_query__WEBPACK_IMPORTED_MODULE_5__] = __webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__);
+
+
 
 
 
@@ -37,22 +42,74 @@ function App({ Component , pageProps  }) {
             }
         }
     });
-    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_tanstack_react_query__WEBPACK_IMPORTED_MODULE_5__.QueryClientProvider, {
-        client: queryClient,
-        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(styled_components__WEBPACK_IMPORTED_MODULE_2__.ThemeProvider, {
-            theme: _styles_theme__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z,
-            children: [
-                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_styles_globalStyles__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, {}),
-                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
-                    ...pageProps
-                })
-            ]
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_redux__WEBPACK_IMPORTED_MODULE_6__.Provider, {
+        store: _store__WEBPACK_IMPORTED_MODULE_7__/* ["default"] */ .Z,
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_tanstack_react_query__WEBPACK_IMPORTED_MODULE_5__.QueryClientProvider, {
+            client: queryClient,
+            children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(styled_components__WEBPACK_IMPORTED_MODULE_2__.ThemeProvider, {
+                theme: _styles_theme__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z,
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_styles_globalStyles__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, {}),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
+                        ...pageProps
+                    })
+                ]
+            })
         })
     });
 }
 
 __webpack_async_result__();
 } catch(e) { __webpack_async_result__(e); } });
+
+/***/ }),
+
+/***/ 9224:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _reduxjs_toolkit__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5184);
+/* harmony import */ var _reduxjs_toolkit__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_reduxjs_toolkit__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _reducer_counter__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(7184);
+
+
+const store = (0,_reduxjs_toolkit__WEBPACK_IMPORTED_MODULE_0__.configureStore)({
+    reducer: {
+        counter: _reducer_counter__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z
+    },
+    devTools: "production" !== "production"
+});
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (store);
+
+
+/***/ }),
+
+/***/ 7184:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__),
+/* harmony export */   "k": () => (/* binding */ setCount)
+/* harmony export */ });
+/* harmony import */ var _reduxjs_toolkit__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5184);
+/* harmony import */ var _reduxjs_toolkit__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_reduxjs_toolkit__WEBPACK_IMPORTED_MODULE_0__);
+
+const slice = (0,_reduxjs_toolkit__WEBPACK_IMPORTED_MODULE_0__.createSlice)({
+    name: "counter",
+    initialState: {
+        count: 0
+    },
+    reducers: {
+        setCount: (state, action)=>{
+            state.count = action.payload;
+        }
+    }
+});
+const { setCount  } = slice.actions;
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (slice.reducer);
+
 
 /***/ }),
 
@@ -122,6 +179,20 @@ const theme = {
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (theme);
 
+
+/***/ }),
+
+/***/ 5184:
+/***/ ((module) => {
+
+module.exports = require("@reduxjs/toolkit");
+
+/***/ }),
+
+/***/ 6022:
+/***/ ((module) => {
+
+module.exports = require("react-redux");
 
 /***/ }),
 
