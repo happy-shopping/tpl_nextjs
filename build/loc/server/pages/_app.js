3a329352e0a1eeb742780294b36a90a88fec159a
우진택 <jt.woo@pping.kr>
2023-04-28 12:12:21 +0900

    styled-component 설치 후 빌드 파일 생성

--- build/loc/server/pages/_app.js
+++ build/loc/server/pages/_app.js
@@ -5,19 +5,82 @@ exports.id = 888;
 exports.ids = [888];
 exports.modules = {
 
-/***/ 212:
+/***/ 199:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+// ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (/* binding */ App)
-/* harmony export */ });
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ App)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+;// CONCATENATED MODULE: external "styled-reset"
+const external_styled_reset_namespaceObject = require("styled-reset");
+var external_styled_reset_default = /*#__PURE__*/__webpack_require__.n(external_styled_reset_namespaceObject);
+// EXTERNAL MODULE: external "styled-components"
+var external_styled_components_ = __webpack_require__(518);
+;// CONCATENATED MODULE: ./src/styles/globalStyles.ts
+
+
+/* harmony default export */ const globalStyles = (external_styled_components_.createGlobalStyle`
+  ${(external_styled_reset_default())}
+  
+    *, *::before, *::after {
+        box-sizing: border-box;
+  }
+
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Noto Sans KR', '나눔고딕', 'Nanum Gothic', '맑은고딕',
+    'Malgun Gothic', ‘Apple SD Gothic Neo’, '돋움', dotum, '굴림', gulim,
+    sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`);
+
+;// CONCATENATED MODULE: ./src/styles/theme.ts
+const colors = {
+    black: "#333333",
+    yellow: "#FF9900",
+    orange: "#F34522",
+    gray: "#666666",
+    gray_background: "#F6F6F6",
+    red: "#E23714",
+    blue: "#418DED",
+    navy: "#005796",
+    white: "#FFFFFF"
+};
+const fonts = {
+};
+const theme = {
+    colors,
+    fonts
+};
+/* harmony default export */ const styles_theme = (theme);
+
+;// CONCATENATED MODULE: ./src/pages/_app.tsx
+
+
+
 
 function App({ Component , pageProps  }) {
-    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
-        ...pageProps
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(external_styled_components_.ThemeProvider, {
+        theme: styles_theme,
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx(globalStyles, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx(Component, {
+                ...pageProps
+            })
+        ]
     });
 }
 
@@ -29,6 +92,13 @@ function App({ Component , pageProps  }) {
 
 module.exports = require("react/jsx-runtime");
 
+/***/ }),
+
+/***/ 518:
+/***/ ((module) => {
+
+module.exports = require("styled-components");
+
 /***/ })
 
 };
@@ -38,7 +108,7 @@ module.exports = require("react/jsx-runtime");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(212));
+var __webpack_exports__ = (__webpack_exec__(199));
 module.exports = __webpack_exports__;
 
 })();
