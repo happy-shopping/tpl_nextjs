33b6090174857ee37f9a423e5eeec13119a1a3b6
이소운 <sw.lee@pping.kr>
2023-06-27 18:58:22 +0900

    styled-component 설치 후 빌드 파일 생성

--- build/dev/server/pages/_app.js
+++ build/dev/server/pages/_app.js
@@ -5,7 +5,7 @@ exports.id = 888;
 exports.ids = [888];
 exports.modules = {
 
-/***/ 302:
+/***/ 7442:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 // ESM COMPAT FLAG
@@ -41,11 +41,46 @@ var module_default = /*#__PURE__*/__webpack_require__.n(pages_module);
 var helpers = __webpack_require__(7182);
 // EXTERNAL MODULE: ./node_modules/react/jsx-runtime.js
 var jsx_runtime = __webpack_require__(5893);
+;// CONCATENATED MODULE: external "styled-reset"
+const external_styled_reset_namespaceObject = require("styled-reset");
+var external_styled_reset_default = /*#__PURE__*/__webpack_require__.n(external_styled_reset_namespaceObject);
+;// CONCATENATED MODULE: external "styled-components"
+const external_styled_components_namespaceObject = require("styled-components");
+;// CONCATENATED MODULE: ./src/styles/globalStyles.ts
+
+
+/* harmony default export */ const globalStyles = (external_styled_components_namespaceObject.createGlobalStyle`
+  ${(external_styled_reset_default())}
+  
+    *, *::before, *::after {
+        box-sizing: border-box;
+  }
+
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Noto Sans KR', '나눔고딕', 'Nanum Gothic', '맑은고딕',
+    'Malgun Gothic', ‘Apple SD Gothic Neo’, '돋움', dotum, '굴림', gulim,
+    sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`);
+
 ;// CONCATENATED MODULE: ./src/pages/_app.tsx
 
+
 function App({ Component, pageProps }) {
-    return /*#__PURE__*/ jsx_runtime.jsx(Component, {
-        ...pageProps
+    return /*#__PURE__*/ (0,jsx_runtime.jsxs)(jsx_runtime.Fragment, {
+        children: [
+            /*#__PURE__*/ jsx_runtime.jsx(globalStyles, {}),
+            /*#__PURE__*/ jsx_runtime.jsx(Component, {
+                ...pageProps
+            })
+        ]
     });
 }
 
@@ -112,7 +147,7 @@ module.exports = require("react");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [812], () => (__webpack_exec__(302)));
+var __webpack_exports__ = __webpack_require__.X(0, [812], () => (__webpack_exec__(7442)));
 module.exports = __webpack_exports__;
 
 })();
