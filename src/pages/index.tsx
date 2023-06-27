35c5dbeb55a1e88bd9d580813cfdb05603705494
이소운 <sw.lee@pping.kr>
2023-06-27 15:46:43 +0900

    Prettier 설치 및 세팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-      <main>
-        <h1>행복쇼핑 Next.js Pages 폴더 구조 템플릿</h1>
-      </main>
-  );
+    <main>
+      <h1>행복쇼핑 Next.js Pages 폴더 구조 템플릿</h1>
+    </main>
+  )
 }
